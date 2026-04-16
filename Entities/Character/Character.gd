3754baa6a2c9936170973c8ec2658d786aa8911e
catster <mirololeo@gmail.com>
2026-04-17 00:24:31 +0100

    Basic Character Outfitting

--- Entities/Character/Character.gd
+++ Entities/Character/Character.gd
@@ -1,12 +1,24 @@
 @tool
 extends CharacterBody3D
+class_name Character
 
 @export var character_colour := Color.WHITE:
 	set(val): set_mesh_col(val); character_colour = val;
 	get: return character_colour
 
-@onready var character_mesh: MeshInstance3D = $MeshInstance3D
+@export var outfit_texture: Texture2D:
+	set(val): set_outfit_texture(val); outfit_texture = val;
+	get: return outfit_texture
+
+var character_mesh: MeshInstance3D
+var outfit_mesh: MeshInstance3D
 
 func set_mesh_col(col: Color):
+	character_mesh = $CharacterBody
 	var mat: StandardMaterial3D = character_mesh.mesh.surface_get_material(0)
 	mat.albedo_color = col
+
+func set_outfit_texture(texture: Texture2D):
+	outfit_mesh = $CharacterBody/Outfit
+	var mat: Material = outfit_mesh.get_active_material(0)
+	mat.albedo_texture = texture
