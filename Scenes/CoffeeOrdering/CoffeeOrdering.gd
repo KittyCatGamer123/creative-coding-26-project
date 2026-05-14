d1b444575f45e61927823cf765a4a07653d15448
catster <mirololeo@gmail.com>
2026-05-14 04:35:40 +0100

    Coffee Shop sequence init

--- Scenes/CoffeeOrdering/CoffeeOrdering.gd
+++ Scenes/CoffeeOrdering/CoffeeOrdering.gd
@@ -1,10 +1,14 @@
 extends Node3D
 
-@onready var pointer: AnimatedSprite2D = $CanvasLayer/HandPointer
+@onready var player_raycast: CollisionShape3D = $Player/Head/Camera3D/InteractionDetection/CollisionShape3D
+@onready var pointer: AnimatedSprite2D = $CanvasLayer/Hand
+
+func _ready() -> void:
+	player_raycast.shape.length = 5
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed:
-			pointer.play("default")
+			pointer.play("point")
 		elif not event.pressed:
-			pointer.play_backwards("default")
+			pointer.play_backwards("point")
